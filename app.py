9a707846fbaa982351929f79bc5ea4f76a78e47f
Sylvain Bourgeois <sylvainoman@gmail.com>
2026-09-11 14:40:25 +0200

    feat: ajout fonction saluer

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def multiplier(a, b):
     return a * b
 
 
+def saluer(nom):
+    return f"Bonjour {nom}"
+
+
 if __name__ == "__main__":
     print("Application démarrée. Seuil configuré :", seuil)
     print("Statut :", get_status())
